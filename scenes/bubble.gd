a4d8640246f83138c92129ad8190e0ad58f24d96
mambo <omiyer76@gmail.com>
2025-01-26 02:56:35 -0600

    start game functionality, art makeover stuff, difficulty progression

--- scenes/bubble.gd
+++ scenes/bubble.gd
@@ -3,31 +3,34 @@ extends CharacterBody2D
 
 const pSpeed = 200.0
 @onready var line = %Line2D
-const lSpeed = 2.0 
-const xThresh = -100
 
 var isBlowing: bool
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if Main.gameRun:
 		if isBlowing || Input.get_axis("ui_up", "ui_down"):
 			velocity.y = -1 * pSpeed
 
-		velocity += get_gravity()*delta*0.4
+		velocity += get_gravity()*delta*0.35
 
-		if position.y < 0 || position.y > get_window().size.y:
+		if position.y < 0 || position.y > (get_window().size.y - 120):
 			Main.stopGame()
-			
+		move_and_slide()
+
 	else:
 		velocity.y = 0;
-	move_and_slide()
 
 
 func _on_main_audio_changed(db_level: Variant) -> void:
-	if db_level > -20:
+	if db_level > Main.soundThresh:
 		isBlowing = true
 	else:
 		isBlowing = false
-
+		
+#func reset():
+	#$AnimatedSprite2D.play("default")
+	#
+	
 func pop():
+	print("dead")
 	$AnimatedSprite2D.play("death")
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,13 +4,22 @@ extends Node
 signal audio_changed(db_level)
 
 var gameRun : bool
+
+var startWaiting : bool
+const scaleAdd : Vector2 = Vector2(0.02, 0.02)
+
 var score
-const scrollSpeed : int = 2
+var scroll
 var pipes : Array
 const pipeDelay : int = 100
 const pipeRange : int = 150
-const pipeGap : int = 200;
 var screen_size : Vector2i
+const soundThresh: int = -20
+
+
+var pipeGap : int = 200
+var scrollSpeed : float = 2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,31 +27,53 @@ func _ready() -> void:
 	newGame()
 
 func newGame():
+	startWaiting = true
 	gameRun = false
 	score = 0
+	scroll = 0
+	pipeGap = 200
+	scrollSpeed = 2
 	pipes.clear()
+	pipes.append($Wand)
 	_on_timer_timeout()
-	startGame() #remove this once blowing bubble start thing is finished
 	
 func startGame():
-	gameRun=true;
+	startWaiting = false
+	gameRun = true;
 	$Timer.start()
 	
-func _input(event): # temp start game condition
-	if !gameRun:
-		if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-				startGame()
-				
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if gameRun:
-		for pipe in pipes:
-			pipe.position.x -= scrollSpeed
-	
 	var db_level = AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Capture"), 0)
 	audio_changed.emit(db_level)
+	if gameRun:
+		scroll += scrollSpeed
+		if scroll>=screen_size.x:
+			scroll=0
+		$Ground.position.x = -scroll
+		for pipe in pipes:
+			pipe.position.x -= scrollSpeed
+	else: 
+		if startWaiting:
+			if db_level > soundThresh:
+				$bubble.scale += scaleAdd
+				if($bubble.scale.x >= 1):
+					$bubble.scale = Vector2(1,1)
+					$bubble.position.x = 120
+					startGame()
+			else:
+				$bubble.scale -= scaleAdd
+				if($bubble.scale.x < 0):
+					$bubble.scale  =Vector2(0,0)
+					
+		#else:
+			#if db_level > soundThresh+10:
+				#get_tree().reload_current_scene()
+
+	
+
 	
 
 func _on_timer_timeout() -> void:
@@ -69,11 +100,32 @@ func _on_timer_timeout() -> void:
 	pipes.append(pipeObj2)
 
 func stopGame():
-	$Timer.stop()
 	gameRun=false
+	$Timer.stop()
 	$bubble.pop()
 
-func scoreIncrement():
+func scoreIncrement(): # scrollSpeed, pipeGap, timer
 	score+=1
-	print(score)
+	
+	if score == 2:
+		scrollSpeed=3
+		$Timer.wait_time=3
+	elif score == 5:
+		scrollSpeed=4
+		$Timer.wait_time=2.8
+	elif score == 7:
+		pipeGap = 165
+	elif score == 10:
+		$Timer.wait_time=2.6
+		scrollSpeed=4.5
+	elif score == 15:
+		scrollSpeed=4.7
+		pipeGap = 140
+	else:
+		if score%5==0 && score!=0:
+			scrollSpeed+=0.3;
+			$Timer.wait_time-=0.1;
+			
+	
+	
 	
